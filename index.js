b9c6c676ea666ff51f49de7ed0e5f93619cec868
Edgar <ed1g2a3r@gmail.com>
2019-07-23 23:30:22 -0600

    biggestObject 2.0

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 // const rutas = require('./src/rutas')
 const user = require('./src/user');
-//const UtilsController = require('./src/UtilsController');
+const UtilsController = require('./src/UtilsController');
 // const client = require('./src/client')
 // const pets = require('./src/pets')
 const app = express();
 const port = 3000;
-
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 app.get('/', (req, res) => res.send('Hola, bienvenidos a myPetShop'));
 
 user(app);
-//UtilsController(app);
+UtilsController(app);
 // client(app);
 // pets(app);
 
